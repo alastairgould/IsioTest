829aee1cb0a7d066963108e4bd8f3d3b9532e925
Alastair Gould <me@alastairgould.co.uk>
2026-05-08 12:05:42 +0100

    Add first test

--- Csharp/GildedRoseTests/GildedRoseTests.cs
+++ Csharp/GildedRoseTests/GildedRoseTests.cs
@@ -14,4 +14,22 @@ public class GildedRoseTests
         app.UpdateQuality();
         Assert.Equal("fixme", Items[0].Name);
     }
+    
+    [Theory]
+    [InlineData("foo")]
+    [InlineData("Aged Brie")]
+    [InlineData("Backstage passes to a TAFKAL80ETC concert")]
+    public void SellInDecreasesByOne_WhenADayPasses(string name)
+    {
+        var (app, items) = CreateGildedRose([GenericItem(name, sellIn: 2)]);
+        
+        app.UpdateQuality();
+        
+        Assert.Equal(1, items[0].SellIn);
+    }
+   
+    private (GildedRose, IList<Item>) CreateGildedRose(IList<Item> Items) => (new GildedRose(Items), Items);
+   
+    private Item GenericItem(string name = "foo", int sellIn = 2, int quality = 0) => new() { Name = name, SellIn = sellIn, Quality = quality };
+
 }
