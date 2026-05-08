b8d7935169aa7664e4cce9272d9bebc3e332e165
Alastair Gould <me@alastairgould.co.uk>
2026-05-08 18:21:31 +0100

    Change if order

--- Csharp/GildedRose/GildedRose.cs
+++ Csharp/GildedRose/GildedRose.cs
@@ -60,9 +60,9 @@ public class GildedRose
             {
                 if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (item.Quality > 0)
+                    if (item.Name != "Sulfuras, Hand of Ragnaros")
                     {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
+                        if (item.Quality > 0)
                         {
                             item.Quality -= 1;
                         }
